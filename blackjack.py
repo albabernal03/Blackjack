f1d9528db96557ede70b27ba5c37d8e781c09560
albabernal03 <ALBABERO@MYUAX.COM>
2021-11-20 10:56:06 +0100

    creo el diccionario cartas e importo la librería choice

---
+++ blackjack.py
@@ -0,0 +1,18 @@
+from random import choice, sample
+
+cartas = {
+chr(0x1f0a1): 11,
+chr(0x1f0a2): 2,
+chr(0x1f03): 3,
+chr(0x1f04): 4,
+chr(0x1f05): 5,
+chr(0x1f06): 6,
+chr(0x1f07): 7,
+chr(0x1f08): 8,
+chr(0x1f09): 9,
+chr(0x1f0aa): 10,
+chr(0x1f0ab): 10,
+chr(0x1f0ad): 10,
+chr(0x1f0ae): 10,
+}
+
