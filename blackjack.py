d8b76118caa8290eab538769b7b0c10c8d72339e
albabernal03 <ALBABERO@MYUAX.COM>
2021-11-23 19:11:04 +0100

    Como a mi no se me imprimían las cartas que era por la terminal, obte por nombrarlas y así solucionar el problema

--- blackjack.py
+++ blackjack.py
@@ -1,52 +1,18 @@
-from random import choice, sample
+from random import choice
 
 cartas = {
-chr(0x1f0a1): 11,
-chr(0x1f0a2): 2,
-chr(0x1f03): 3,
-chr(0x1f04): 4,
-chr(0x1f05): 5,
-chr(0x1f06): 6,
-chr(0x1f07): 7,
-chr(0x1f08): 8,
-chr(0x1f09): 9,
-chr(0x1f0aa): 10,
-chr(0x1f0ab): 10,
-chr(0x1f0ad): 10,
-chr(0x1f0ae): 10,
+'A': 11,
+'2': 2,
+'3': 3,
+'4': 4,
+'5': 5,
+'6': 6,
+'7': 7,
+'8': 8,
+'9': 9,
+'10': 10,
+'J': 10,
+'Q': 10,
+'K': 10,
 }
 
-# Añado un print para que lea las cartas y la muestre por la terminal de salida 
-
-print("Cartas: {}".format(" ".join(cartas.keys())))
-print("Puntos: {}".format(list(cartas.values())))
-
-# El print que añadimos ahora nos le las cartas de una forma ordenada con su valor al lado
-
-print("1\ Iteración estándar sobre un diccionario")
-for carta, valor in cartas.items():
-    print("la carta {} vale {}".format(carta, valor))
-
-print("2\ Iteración ordenada sobre un diccionario")
-for carta in sorted(cartas.keys()):
-    print("la carta {} vale {}".format(carta, cartas[carta]))
-
-
-print("3\ Black Jack")
-lista_cartas = list(cartas)
-
-print("Ha seleccionado:", end=" ")
-carta = choice(lista_cartas)
-score = cartas[carta]
-print(carta, end=" ")
-carta = choice(lista_cartas)
-score += cartas[carta]
-print(carta, end=" ")
-print(" >>> su puntuación es de", score)
-
-main_banca = sample(lista_cartas, 2)
-score_banca = sum(cartas[carta] for carta in main_banca)
-print("La banca tiene: {} {}  >> su score es {}".format(main_banca[0], main_banca[1], score_banca))
-
-def comparacion ():
-    
